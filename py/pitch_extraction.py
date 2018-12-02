511f4eb942cc896fd9a006d8b0b7df4e56ac8426
w-shimaya <integral2d@gmail.com>
2018-12-02 10:03:10 +0900

    implemented pitch extraction with fft but autocorrelation methods is better

--- py/pitch_extraction.py
+++ py/pitch_extraction.py
@@ -3,14 +3,35 @@ import scipy.signal as sig
 import scipy.io.wavfile as wio
 import matplotlib.pyplot as plt
 
+# pitch extraction using fourier transform
+def pitch_f(data, Fs):
+    size, = data.shape
+    # window
+    window = np.hamming(size)
+    data = data * size
+    # spectrum
+    F = np.fft.fft(data)
+    # power spectrum
+    power = np.abs(F)
+    power = power * power
+
+    # debug view
+    # plt.plot(power)
+    # plt.show()
+
+    return np.argmax(power[:size//2]) * float(Fs) / size
+
 # returns the fundamental frequency[Hz]
 # data: numpy 1d array
 # Fs:   sampling rate[Hz]
-def pitch(data, Fs, outImage=False):
+def pitch(data, Fs, method=None, outImage=False):
     if data.ndim != 1:
         raise TypeError('data must be 1d numpy array')
     # int array -> float array
     data = data.astype('float32')
+
+    if method == 'fft':
+        return pitch_f(data, Fs)
     
     N = data.shape[0]
     # Normalized Autocorrelation(v)
@@ -44,26 +65,31 @@ def pitch(data, Fs, outImage=False):
     F0 = 1.0 / delay
     return F0
 
+
+
 # returns the temporal change of the fundamental frequency
-# data: 1d numpy array
-# Fs:   sampling rate[Hz]
+# data:  1d numpy array
+# Fs:    sampling rate[Hz]
+# N:     width of the window
+# shift: 
 def track_pitch(data, Fs, N, shift):
     length = data.shape[0]
     ret = np.zeros((length - N) / shift) 
     t_axis = np.zeros((length - N) / shift)
     for i in range(ret.shape[0]):
-        ret[i] = pitch(data[i * shift: i * shift + N], Fs)
+        ret[i] = pitch(data[i * shift: i * shift + N], Fs) #, method='fft')
         t_axis[i] = i * float(shift) / Fs
     return ret, t_axis
 
 if __name__ == '__main__':
     Fs, data = wio.read('../data/wav/doremi.wav')
     data = data[:, 0] * 0.5 + data[:, 1] * 0.5
-    ch, time = track_pitch(data, Fs, 1000, 500)
+    ch, time = track_pitch(data, Fs, 4000, 1000)
     plt.figure()
     plt.yscale('log')
     plt.plot(time, ch)
     plt.title('pitch')
     plt.xlabel('time')
     plt.ylabel('pitch[Hz]')
+
     plt.show()
